2be85f1f8370e8660500b22b31380af13ef60e95
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-16 18:07:48 +0100

    orthographe

--- routes/deliverooBack.js
+++ routes/deliverooBack.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const token = process.env.TOKEN;
 const router = express.Router();
 
-router.get("/deliveroBack", async(req, res) => {
+router.get("/deliverooBack", async(req, res) => {
     try {
        const response = await axios.get("https://lereacteur-bootcamp-api.herokuapp.com/api/deliveroo/menu/paris/3eme-temple/sub-arc-subway-rambuteau?day=today&geohash=u09wj8rk5bqr&time=ASAP", {
   headers: {
