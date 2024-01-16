caa08d90ebbd5383619e33315e0e3c1913425201
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-01-16 14:43:56 +0100

    token

---
+++ routes/deliveroo-back.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const token = process.env.TOKEN;
+
+router.get("/deliveroo", async(req, res) => {
+    try {
+       const response = await axios.get("https://lereacteur-bootcamp-api.herokuapp.com/api/deliveroo/menu/paris/3eme-temple/sub-arc-subway-rambuteau?day=today&geohash=u09wj8rk5bqr&time=ASAP", {
+  headers: {
+    'Authorization': `Bearer ${token}` 
+  }
+});
+    res.json(response.data);
+    } catch (error) {
+        res.json({error : error.message})
+    }
+
+});
+
+
+module.exports= router;
