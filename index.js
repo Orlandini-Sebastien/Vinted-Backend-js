2e96ca5d6c18b7fe9d2f1f41d0643ae5c3f56c58
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-04 15:27:21 +0100

    true middlewars

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
 
 const mongoose = require("mongoose");
 app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI);
+
+
+
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
 const offerRoutes = require("./routes/offer");
