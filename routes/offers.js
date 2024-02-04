70edbd9fe8f223458e73b7b9246e5406c23eda45
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-04 15:06:06 +0100

    give acces

--- routes/offers.js
+++ routes/offers.js
@@ -7,6 +7,9 @@ app.use(cors());
 
 router.get("/offers", async(req,res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     try {
         const filters = {};
         let offers={};
