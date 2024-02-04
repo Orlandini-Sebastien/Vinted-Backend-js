9ba8657f93801ae3cf6dfeaa5955f4d16ce3e27a
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-04 16:39:19 +0100

    payment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
 
+const express = require('express');
 const cors = require("cors");
+const stripe = require ("stripe")(process.env.SRIPE_SECRET_KEY)
+const app = express();
+app.use(express.json());
 app.use(cors());
+require('dotenv').config();
 
 const mongoose = require("mongoose");
-app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI);
 
 
@@ -18,11 +19,35 @@ app.use(offerRoutes);
 const offersRoutes = require("./routes/offers");
 app.use(offersRoutes);
 
+app.post("/payment", async (req, res) => {
+  // Réception du token créer via l'API Stripe depuis le Frontend
+  const stripeToken = req.body.stripeToken;
+  // Créer la transaction
+  const response = await stripe.charges.create({
+    amount: 2000,
+    currency: "eur",
+    description: "La description de l'objet acheté",
+    // On envoie ici le token
+    source: stripeToken,
+  });
+  console.log(response.status);
+
+  // TODO
+  // Sauvegarder la transaction dans une BDD MongoDB
+
+  res.json(response);
+});
+
+
+
+
 const deliverooRoutes = require("./routes/deliveroo");
 app.use(deliverooRoutes);
 
 const deliverooBackRoutes = require("./routes/deliverooBack");
 app.use(deliverooBackRoutes);
+
+
 
 app.all("*", (req,res)=>{
     res.status(404).json({message: "😞 Not found 😞"});
