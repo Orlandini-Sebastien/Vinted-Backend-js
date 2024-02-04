598a9d5572e6266f648227f1cd951d8f6ef2f9b0
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-04 17:32:11 +0100

    without payment

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const cors = require("cors");
 
+/*
 const creatStripe = require ("stripe")
 const stripe = createStripe(process.env.SRIPE_SECRET_KEY)
-
+*/
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://thriving-medovik-6bc46e.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -30,6 +31,7 @@ app.use(offerRoutes);
 const offersRoutes = require("./routes/offers");
 app.use(offersRoutes);
 
+/*
 app.post("/payment", async (req, res) => {
   // Réception du token créer via l'API Stripe depuis le Frontend
   const stripeToken = req.body.stripeToken;
@@ -48,7 +50,7 @@ app.post("/payment", async (req, res) => {
 
   res.json(response);
 });
-
+*/
 
 
 
