bfc421abe9884aa18ea6671bd19b36f780509332
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-04 15:33:28 +0100

    give acces

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', ['*', 'https://thriving-medovik-6bc46e.netlify.app' ]);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
