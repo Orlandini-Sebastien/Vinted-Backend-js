650b0e8bd025c10cb6262c21cd74b6a831cd19a0
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-04 17:27:34 +0100

    sans slash

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const creatStripe = require ("stripe")
 const stripe = createStripe(process.env.SRIPE_SECRET_KEY)
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://thriving-medovik-6bc46e.netlify.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://thriving-medovik-6bc46e.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
