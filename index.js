13fd3b2f88057e7aa3353e5dc8a116030e8fcde2
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-05 10:39:09 +0100

    maj npm

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 
 const express = require('express');
 const cors = require("cors");
-
-
-const stripe  = require ("stripe")(process.env.SRIPE_SECRET_KEY)
-
-const isAuthenticated = require("./middlewars/isAuthenticated");
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -20,37 +14,14 @@ const userRoutes = require("./routes/user");
 app.use(userRoutes);
 const offerRoutes = require("./routes/offer");
 app.use(offerRoutes);
-
 const offersRoutes = require("./routes/offers");
 app.use(offersRoutes);
-
-
-app.post("/payment", isAuthenticated, async (req, res) => {
-
-  // Réception du token créer via l'API Stripe depuis le Frontend
-  const stripeToken = req.body.stripeToken;
-  // Créer la transaction
-  const response = await stripe.charges.create({
-    amount: 2000,
-    currency: "eur",
-    description: "La description de l'objet acheté",
-    // On envoie ici le token
-    source: stripeToken,
-  });
-  console.log(response.status);
-
-  // TODO
-  // Sauvegarder la transaction dans une BDD MongoDB
-
-  res.json(response);
-});
-
-
+const paymentRoutes = require("./routes/payment");
+app.use(paymentRoutes);
 
 
 const deliverooRoutes = require("./routes/deliveroo");
 app.use(deliverooRoutes);
-
 const deliverooBackRoutes = require("./routes/deliverooBack");
 app.use(deliverooBackRoutes);
 
---
+++ routes/payment.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const stripe  = require ("stripe")(process.env.SRIPE_SECRET_KEY)
+
+const isAuthenticated = require("../middlewars/isAuthenticated");
+
+
+router.post("/payment", isAuthenticated, async (req, res) => {
+    const stripeToken = req.body.stripeToken;
+    const response = await stripe.charges.create({
+      amount: 2000,
+      currency: "eur",
+      description: "La description de l'objet acheté",
+ 
+      source: stripeToken,
+    });
+    console.log(response.status);
+  
+  
+    res.json(response);
+  });
+  
+  module.exports= router;
