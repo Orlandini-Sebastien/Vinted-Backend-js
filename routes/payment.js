f8d6c0a74cd53249c6b57652875de923116dd425
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-06 13:43:36 +0100

    *

--- routes/payment.js
+++ routes/payment.js
@@ -8,9 +8,8 @@ const isAuthenticated = require("../middlewars/isAuthenticated");
  
 
 router.post("/payment", isAuthenticated,  async (req, res) => {
-  res.set('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', "https://thriving-medovik-6bc46e.netlify.app");
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     const stripeToken = req.body.stripeToken;
